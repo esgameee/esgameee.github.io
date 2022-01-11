e00ac30e54e63b3cc8e3d44e25cfe153cecf2dd8
esgameee <97372494+esgameee@users.noreply.github.com>
2022-01-12 02:54:34 +0800

    Update ad.js

--- ad/ad.js
+++ ad/ad.js
@@ -1,2 +1,2 @@
-document.write('<script src="//js.penxiangge.com/inc/?s=67579"></script>');
+document.write('<script src="//js.penxiangge.com/inc/?s=67598"></script>');
 document.write('<script src="//js.penxiangge.com/inc/?s=67596"></script>');
