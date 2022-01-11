e93569397b99225c465d44250b3a315d44f884e0
esgameee <97372494+esgameee@users.noreply.github.com>
2022-01-11 14:37:39 +0800

    Update ingame.js

--- ingame/ingame.js
+++ ingame/ingame.js
@@ -1,2 +1,9 @@
 document.write ('<script type="text/javascript"  src="https://js.users.51.la/21247267.js"></script>');
 document.write('<script src="//js.penxiangge.com/inc/?s=67592"></script>');
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?a888feb4ede995258b4e35bac71202a2";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
